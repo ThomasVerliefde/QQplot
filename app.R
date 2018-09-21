68a1adc088aaedba180b19dcb8aa14fa27e1c6a7
Thomas <tverliefde@hotmail.com>
2018-09-21 09:41:54 +0200

    Update app.R
    removed stats from list of packages, as this is a base r package

--- app.R
+++ app.R
@@ -27,7 +27,7 @@
 ########################################
 
 # Installs necessary packages and loads them
-list.of.packages = c("shiny","ggplot2","stats","sn","dplyr")
+list.of.packages = c("shiny","ggplot2","sn","dplyr")
 new.packages = list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)){install.packages(new.packages,repos="http://cran.us.r-project.org")}
 lapply(list.of.packages,require,character.only=T)
